8a24678902ab23cdec378b8904fad87792d60630
Md Asaduzaman <asadasad9282@gmail.com>
2023-08-19 00:47:28 +0600

    Improved Functionality with catchAsync and Validation for AcademicSemesters

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# University Management Core Service
+## University Management Core Service
+
+## Techonology
+
+- Typescript
+- Express
+- Jsonwebtoken
+- Zod
+- Jsonwebtoken
+- Bcrypt
+- Cookie-parser
+- Prisma
+- PostgreSQL
+
+## Routes
+
+- /api/v1/academic-semesters (Post)
+
+```
+{
+   "title": "Fall",
+    "year": "2050",
+    "code": "03",
+    "startMonth": "January",
+    "endMonth": "June"
+}
+
+```
--- src/app/modules/academicSemister/academicSemester.route.ts
+++ src/app/modules/academicSemister/academicSemester.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 import { AcademicSemesterController } from './academicSemestet.controller';
 
 const route = express.Router();
 
-route.post('/', AcademicSemesterController.createSemester);
+route.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.createSemester
+);
 
 export const AcademicSemesterRoutes = route;
