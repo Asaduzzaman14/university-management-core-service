b062a19d01e4e70698b55a0b5651dceeeac923ac
Md Asaduzaman <asadasad9282@gmail.com>
2023-08-25 00:48:43 +0600

    course delete get and update route

--- src/app/modules/course/course.controller.ts
+++ src/app/modules/course/course.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { courseFilterableFields } from './course.constants';
 import { CourseServices } from './course.service';
 
 const createCourse = catchAsync(async (req: Request, res: Response) => {
@@ -15,6 +17,44 @@ const createCourse = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllCourses = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, courseFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await CourseServices.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Courses fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getCourseById = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await CourseServices.getCourseById(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course fetched successfully',
+    data: result,
+  });
+});
+
+const deleteCourse = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await CourseServices.deleteCourse(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course deleted successfully',
+    data: result,
+  });
+});
+
 export const CourseController = {
   createCourse,
+  getAllCourses,
+  getCourseById,
+  deleteCourse,
 };
--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -3,6 +3,12 @@ import { CourseController } from './course.controller';
 
 const router = express.Router();
 
+router.get('/:id', CourseController.getAllCourses);
+
+router.delete('/:id', CourseController.deleteCourse);
+
+router.get('/', CourseController.getAllCourses);
+
 router.post('/', CourseController.createCourse);
 
 export const CourseRoutes = router;
