bd3b504a582e978da18703dcfaef79b5ffbbf9bd
Md Asaduzaman <asadasad9282@gmail.com>
2023-09-12 10:35:54 +0600

    Creating Class Schedules with Offered Course Section Updates

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -2,8 +2,12 @@ import { OfferedCourseSection } from '@prisma/client';
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
+import { asyncForEach } from '../../../shared/utils';
+import { OfferedCourseClassScheduleUtils } from './../offeredCourseClassSchedule/offeredCourseClassSchedule.utils';
 
-const insertInToDB = async (data: any): Promise<OfferedCourseSection> => {
+const insertInToDB = async (payload: any): Promise<OfferedCourseSection> => {
+  const { classSchedule, ...data } = payload;
+
   const isExistOfferedCourse = await prisma.offeredCourse.findFirst({
     where: {
       id: data.offeredCourseId,
@@ -16,10 +20,37 @@ const insertInToDB = async (data: any): Promise<OfferedCourseSection> => {
   console.log(data, 'this is data');
 
   data.semesterRegistrationId = isExistOfferedCourse.semesterRegistrationId;
-  const result = await prisma.offeredCourseSection.create({
-    data,
+
+  await asyncForEach(classSchedule, async (schedule: any) => {
+    await OfferedCourseClassScheduleUtils.checkRoomAvailable(schedule);
+    await OfferedCourseClassScheduleUtils.checkFacultyAvailable(schedule);
   });
-  return result;
+
+  const createSection = prisma.$transaction(async trancationClient => {
+    const createOfferedCourseSection =
+      await trancationClient.offeredCourseSection.create({
+        data,
+      });
+
+    const scheduleData = classSchedule.map((schedule: any) => ({
+      startTime: schedule.startTime,
+      endTime: schedule.endTime,
+      dayOfWeek: schedule.dayOfWeek,
+      roomId: schedule.roomId,
+      facultyId: schedule.facultyId,
+      offeredCourseSectionId: createOfferedCourseSection.id,
+      semesterRegistrationId: isExistOfferedCourse.semesterRegistrationId,
+    }));
+
+    const createSchedule =
+      await trancationClient.offeredCourseClassSchedule.createMany({
+        data: scheduleData,
+      });
+
+    return createSchedule;
+
+    // console.log(scheduleData);
+  });
 };
 
 export const OfferedCourseSectionServices = {
