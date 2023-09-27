b454aba8af9e224cca41a13fe675718d090961fc
Md Asaduzaman <asadasad9282@gmail.com>
2023-09-28 02:36:47 +0600

    changes

--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -1,6 +1,8 @@
+import initFacultyEvents from '../modules/faculty/faculty.events';
 import initStudentEvents from '../modules/student/student.events';
 
 const subscriveToEvents = () => {
   initStudentEvents();
+  initFacultyEvents();
 };
 export default subscriveToEvents;
